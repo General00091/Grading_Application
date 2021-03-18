96d34321e18fb71808fcf5a2d1bead5b242c844e
General00091 <isabar404@gmail.com>
2021-03-18 14:26:45 -0500

    erased unnecessary code + created new page

---
+++ Redirect.php
@@ -0,0 +1,12 @@
+<?php
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $db = 'grading application';
+    $connection = mysqli_connect($host, $user, $pass, $db);
+    $ProjectID = mysqli_real_escape_string($connection, $_REQUEST['projectID']);
+    $ProjectID = $_REQUEST['projectID'];
+    
+    header('Location:https://www.youtube.com/watch?v=Ra6rta-NNbM')
+?>
+
