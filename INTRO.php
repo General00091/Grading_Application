2bcb43fe776aa85f075a78ea72cde5ac56e0bf19
General00091 <isabar404@gmail.com>
2021-04-28 11:52:34 -0500

    added comments and centered input of project ID

--- INTRO.php
+++ INTRO.php
@@ -1,38 +1,43 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <!-- title soon to change...hopefully -->
     <title>Project001.site</title>
     <style>
+    /* form is used to style where to input project ID and the submit button on the page */
     form {padding-top: 120px;
     text-align: center;
     font-size: 30px;
     }
-    input{width: 250px;
+    /* input is used to style the input of project ID */
+    input {width: 250px;
       height: 40px;
       font-size: 30px
     }
+    /* body is used to style body text and color */
     body {
       font-family: Arial, sans-serif;
       background-color: #E0EBE5;
     }
+    /* button is used to style the submit button */
     button {
       text-align: center;
       font-size: 16px;
       padding: 15px 32px;
       margin: 4px 2px;
     }
+
     </style>
   </head>
 <body>
-
+ <!-- title size and position of the title text -->
 <h1 style="font-size:300%;">
 <h1 style="text-align:center;">HSA McKinley Park Science Fair Grader</h1>
-
+<form>
   Project ID:<br>
   <input type="number" name="projectID" required>
   <br><br>
     <button type="submit" formaction="Redirect.php">Submit</button>
 </form>
-
 </body>
 </html>
